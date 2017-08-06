adb6d80d11443ecac61ee4fb65a47f2ebb3fe4e3
aaron-stewart <aaron.stewart.contact@gmail.com>
2017-08-05 20:46:48 -0500

    7-assignment complete. Create playSong function. Write Documentation for SongPayer service attributes and functions.

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,7 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
-
+        
         var currentSong = null;
         /**
         * @desc Buzz object audio file
@@ -27,19 +27,36 @@
 
             currentSong = song;
         };
+        /**
+        * @function playSong
+        * @desc Play a song
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
 
+        /**
+        * @function play
+        * @desc Play current or new song
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
                 }
             }
         };
-
+        /**
+        * @function pause
+        * @desc Pause current song
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
